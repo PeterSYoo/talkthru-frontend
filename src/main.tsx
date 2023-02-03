1f7fbe3ee110179736d6ca25c299554baff63f8c
Peter Yoo <trustintofu@gmail.com>
2023-02-03 01:11:53 -0800

    feat(main): Creates routes for index page and room/:id dynamic route using react-router-dom.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { RoomProvider } from './contexts/RoomContext';
 import './index.css';
+import { HomePage } from './pages/Home.pages';
+import { RoomPage } from './pages/Room.pages';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <RoomProvider>
+        <div className="h-screen w-screen">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/room/:id" element={<RoomPage />} />
+          </Routes>
+        </div>
+      </RoomProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
