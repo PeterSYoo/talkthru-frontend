fb42502e3edb8de109e56c455dda5594961749a7
Peter Yoo <trustintofu@gmail.com>
2023-02-05 00:20:23 -0800

    feat(Room):  Adds semantic comments to the code for better understanding.

--- src/pages/Room.pages.tsx
+++ src/pages/Room.pages.tsx
@@ -1,14 +1,34 @@
 import { useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { VideoPlayer } from '../components/VideoPlayer.components';
+import { PeersState } from '../contexts/peersReducer';
 import { RoomContext } from '../contexts/RoomContext';
 
 export const RoomPage = () => {
+  // Destructure the `id` from the URL parameters using `useParams` hook.
   const { id } = useParams();
-  const { ws } = useContext(RoomContext);
 
+  // Destructure the required values from the `RoomContext` using the `useContext` hook.
+  const { ws, me, stream, peers } = useContext(RoomContext);
+
+  // Use the `useEffect` hook to join the room when the `me` object is available.
   useEffect(() => {
-    ws.emit('join-room', { roomId: id });
-  }, [id]);
+    // If the `me` object is available, emit a 'join-room' event with the `roomId` and `peerId` to the WebSocket.
+    if (me) ws.emit('join-room', { roomId: id, peerId: me._id });
+  }, [id, me, ws]);
 
-  return <>Room id {id}</>;
+  return (
+    <>
+      Room id {id}
+      <div className="">
+        <VideoPlayer stream={stream} />
+        <div className="grid grid-cols-4 gap-4">
+          {peers &&
+            Object.values(peers as PeersState).map((peer) => (
+              <VideoPlayer stream={peer.stream} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
 };
