2a89867893e6943107256ee8a00e91f31e6c49cf
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-09 13:13:47 -0800

    feat(Room.pages): Adds screen sharing functionality with some styling that dynamically changes depending on screen sharing state.

--- src/pages/Room.pages.tsx
+++ src/pages/Room.pages.tsx
@@ -23,20 +23,37 @@ export const RoomPage = () => {
     setRoomId(id);
   }, [id, setRoomId]);
 
+  // Ternary to determine which peer is sharing their screen
+  // If there isn't a screenSharingId or screenSharingId matches local peer's id -> use 'stream' in state
+  const screenSharingVideo = screenSharingId === me?.id ? stream : peers[screenSharingId]?.stream;
+
+  // Destructure 'peers' state object
+  const { [screenSharingId]: sharing, ...peersToShow } = peers;
+
   return (
     <>
       Room id {id}
-      <div className="">
-        <div className="grid grid-cols-4 gap-4">
-          <VideoPlayer stream={stream} />
+      <div className="flex">
+        {/* Primary screen */}
+          <div className="w-4/5 pr-4">
+            {/* Conditionally sets primary screen to the shared video or local user's video */}
+            <VideoPlayer stream={screenSharingVideo || stream} />
+          </div>
+
+        {/* Peer screens */}
+        <div className="w-1/5 grid gap-4 grid-col-1">
+          {/* Includes local stream in-line with peers if it isn't being shared */}
+          {screenSharingId && screenSharingId !== me?.id &&
+            <VideoPlayer stream={stream} />
+          }
           {peers &&
-            Object.values(peers as PeersState).map((peer) => (
+            Object.values(peersToShow as PeersState).map((peer) => (
               <VideoPlayer stream={peer.stream} />
-            ))}
+          ))}
         </div>
-        <div className="fixed bottom-0 p-6 w-full flex justify-center border-t-2">
-          <ShareScreenButton onClick={shareScreen}/>
-        </div>
+      </div>
+      <div className="fixed bottom-0 p-6 w-full flex justify-center border-t-2">
+        <ShareScreenButton onClick={shareScreen}/>
       </div>
     </>
   );
