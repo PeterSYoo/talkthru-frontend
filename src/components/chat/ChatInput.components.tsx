a50dac5ee5e9ac21a0cebdd98d01dd9ea856db12
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-09 22:32:22 -0800

    feat(ChatInput): Adds logic to handle passing messages up to 'RoomContext'.

--- src/components/chat/ChatInput.components.tsx
+++ src/components/chat/ChatInput.components.tsx
@@ -1,12 +1,21 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
+import { RoomContext } from "../../contexts/RoomContext";
 
 export const ChatInput: React.FC = () => {
   // State for user's chat input
   const [message, setMessage] = useState<string>("");
+  // Deconstruct the props we need from RoomContext
+  const { sendMessage } = useContext(RoomContext);
 
   return (
     <div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage(message);
+          setMessage("");
+        }}
+      >
         <div className="flex">
           <textarea
             className="border rounded"
