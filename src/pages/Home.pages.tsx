5fbd6594b5ec2c393c3d74860dfd4ab05be090a5
aparks4 <arynmparks@gmail.com>
2023-02-13 12:47:33 -0800

    feat(Home.pages.tsx): adds h3 displaying email of the current user and logout btn

--- src/pages/Home.pages.tsx
+++ src/pages/Home.pages.tsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db, logout } from '../firebase';
+import { query, getDocs, collection, where } from 'firebase/firestore';
+import { useNavigate } from 'react-router-dom';
 import { CreateButton } from '../components/CreateButton.components';
 
 export const HomePage = () => {
+  const [user, loading, error] = useAuthState(auth);
+
+  const navigate = useNavigate();
+
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate('/');
+  }, [user, loading]);
+
   return (
     <>
       <div className="w-full h-full bg-blue-100 flex flex-col justify-center items-center">
+        <h3>Logged in as {user?.email}</h3>
         <CreateButton />
+        <button onClick={logout}>Logout</button>
       </div>
     </>
   );
