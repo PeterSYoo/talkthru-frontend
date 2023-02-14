ce3c0f76dc7e829fe4d5a24409961edf0fa5445e
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-14 14:24:13 -0800

    feat(ChatBubble): Adds author and timestamp to messages with some styling.

--- src/components/chat/ChatBubble.components.tsx
+++ src/components/chat/ChatBubble.components.tsx
@@ -10,14 +10,26 @@ interface IMessage {
 
 export const ChatBubble: React.FC<{message: IMessage}> = ({message}) => {
   // Get state for local peer to dynamically set styling depending on message's author
-  const { me } = useContext(RoomContext)
+  const { me, peers } = useContext(RoomContext)
+  const author = message.author && peers[message.author];
+  const authorName = author?.userName || "anonymous";
+  const time = new Date(message.timestamp).toLocaleTimeString();
+
   // Uses optional chaining to make sure 'me' exists before reading it's properties to prevent a TypeError
   const isSelf = message.author === me?.id;
 
   return (
     <div className={`m-2 flex ${isSelf ? "pl-10 justify-end" : "pr-10 justify-start"}`}>
-      <div className={`inline-block py-2 px-4 rounded ${isSelf ? "bg-gray-200" : "bg-gray-400"}`}>
-        {message.content}
+      <div className="flex flex-col">
+        <div className={`inline-block py-2 px-4 rounded ${isSelf ? "bg-gray-200" : "bg-gray-400"}`}>
+          {message.content}
+          <div className={`text-xs opacity-50 ${isSelf ? "text-right" : "text-left"}`}>
+            {time}
+          </div>
+        </div>
+        <div className={`text-xs ${isSelf ? "text-right" : "text-left"}`}>
+          {isSelf ? "You" : authorName}
+        </div>
       </div>
     </div>
   );
