58ed3783c45173571afa4b418bc98817099a98d2
Peter Yoo <trustintofu@gmail.com>
2023-02-16 22:58:47 -0800

    feat(WaitingRoom): Adds choose subject component.

--- src/pages/WaitingRoom.pages.tsx
+++ src/pages/WaitingRoom.pages.tsx
@@ -1,16 +1,19 @@
-import { CreateButton } from "../components/CreateButton.components";
+import { CreateButton } from '../components/CreateButton.components';
+import { ChooseSubject } from '../components/waiting-room/ChooseSubject.components';
 
 export const WaitingRoomPage = () => {
-    // Choose subject and experience level for new chat
-    // Then loading animation
-    // Then match
-    // Then navigate to "/room/:id"
-    return (
-        <>
-            <h1>Waiting Room Page</h1>
-            <div className="w-full h-full bg-blue-100 flex flex-col justify-center items-center">
-                <CreateButton />
-            </div>
-        </>
-    );
+  // Choose subject and experience level for new chat
+  // Then loading animation
+  // Then match
+  // Then navigate to "/room/:id"
+  return (
+    <>
+      <div className="h-screen w-full font-poppins pt-[88px]">
+        {/* Choose a Subject */}
+        <ChooseSubject />
+        {/*  */}
+        {/* <CreateButton /> */}
+      </div>
+    </>
+  );
 };
