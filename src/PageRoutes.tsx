c924fcfc770cf9c2b2e64e2f9c5d4415e9b03aeb
Peter Yoo <trustintofu@gmail.com>
2023-02-18 20:17:17 -0800

    refactor(PageRoutes): Conditionally renders header based root route. Changes waitingroom route name to premeet.

--- src/PageRoutes.tsx
+++ src/PageRoutes.tsx
@@ -1,32 +1,35 @@
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { ChatProvider } from './contexts/ChatContext';
-import { SplashPage } from './pages/Splash.pages';
+import { LandingPage } from './pages/Landing.pages';
 import { LoginPage } from './pages/Login.pages';
 import { ProfilePage } from './pages/Profile.pages';
 import { HomePage } from './pages/Home.pages';
 import { RoomPage } from './pages/Room.pages';
 import { SignUpPage } from './pages/SignUp.pages';
 import { PostMeetingPage } from './pages/PostMeeting.pages';
-import { WaitingRoomPage } from './pages/WaitingRoom.pages';
+import { PreMeetingPage } from './pages/PreMeeting.pages';
 import { ResetPage } from './pages/Reset.pages';
 import { Header } from './components/Header.components';
 
 export const PageRoutes = () => {
-  const headerRoutes = ['/', '/login', '/signup', '/waitingroom'];
   const location = useLocation();
-  const shouldShowHeader = headerRoutes.includes(location.pathname);
+  const isNotRoot = location.pathname !== '/';
 
   return (
-    <div className="">
-      {shouldShowHeader && <Header />}
+    <div
+      className={`${
+        isNotRoot && 'grid grid-rows-[88px_1fr] h-screen'
+      } font-poppins`}
+    >
+      {isNotRoot && <Header />}
       <Routes>
-        <Route path="/" element={<SplashPage />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
         <Route path="/reset" element={<ResetPage />} />
         <Route path="/profile/:id" element={<ProfilePage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/waitingroom" element={<WaitingRoomPage />} />
+        <Route path="/premeet" element={<PreMeetingPage />} />
         <Route
           path="/room/:id"
           element={
