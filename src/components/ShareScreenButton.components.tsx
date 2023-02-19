c7aa032eac9dd648588849681d51e65d8bd7a8d9
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-19 14:01:50 -0800

    feat(ShareScreen.components, ChatButton.components): Adds new images to represent components.

--- src/components/ShareScreenButton.components.tsx
+++ src/components/ShareScreenButton.components.tsx
@@ -5,12 +5,15 @@ interface ShareScreenButtonProps {
 
 export const ShareScreenButton: React.FC<ShareScreenButtonProps> = ({onClick, screenSharingId}) => {
   return (
-    <button
-      className={`py-2 px-4 mx-2 rounded-lg text-xl text-white ${
-        screenSharingId ? "bg-red-400 hover:bg-red-600" : "bg-green-400 hover:bg-green-600"}`}
-      onClick={onClick}
-    >
-      {screenSharingId ? "Stop Sharing" : "Start Sharing"}
-    </button>
-  )
+		<button
+			className={`py-2 px-4 mx-2 rounded-lg text-xl text-white ${
+				screenSharingId ? 'bg-red-400 hover:bg-red-600' : 'bg-green-400 hover:bg-green-600'
+			}`}
+			onClick={onClick}>
+			<img
+				src='https://res.cloudinary.com/dryh1nvhk/image/upload/v1676592008/TalkThru/Meeting%20Room%20Page/share_screen_bjiv34.png'
+				alt='screen-share-icon'
+			/>
+		</button>
+	);
 }
