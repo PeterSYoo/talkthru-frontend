64a655eb8fd445fbf31aa6d0c68b7194579a10f2
Peter Yoo <trustintofu@gmail.com>
2023-02-18 20:18:46 -0800

    feat(ChatStudyConnect): Adds animation to down arrow. Refactors heading and paragraph.

--- src/components/landing-page/ChatStudyConnect.components.tsx
+++ src/components/landing-page/ChatStudyConnect.components.tsx
@@ -8,18 +8,15 @@ export const ChatStudyConnect = () => {
             {/* Info Text */}
             <div>
               {/* Heading */}
-              <h1 className="text-[60px] font-bold max-w-[370px] leading-[70.38px]">
+              <h1 className="text-[60px] font-bold max-w-[429px] leading-[70.38px]">
                 Chat
-                <span className="text-[#E4E325]">.</span> Study
-                <span className="text-[#E4E325]">.</span> Connect
                 <span className="text-[#E4E325]">.</span>
               </h1>
               {/* Paragraph */}
-              <p className="max-w-[509px] text-[22px] font-medium leading-[25.81px] pt-[66px]">
-                Need support when it comes to your
-                <br /> studies? Want to work things through with a<br /> peer?
-                Create an account to access our service
-                <br /> and get connected with someone now!
+              <p className="max-w-[509px] text-[22px] font-medium leading-[25.81px] pt-[88px]">
+                Need support when it comes to your studies? Want to work things
+                through with a peer? Create an account to access our service and
+                get connected with someone now!
               </p>
               {/* Sign Up Button */}
               <button className="w-[395px] h-[50px] border-2 border-[#F1F192] bg-[#F1F192] rounded-[30px] flex justify-center items-center text-[25px] font-medium mt-[47px]">
@@ -36,10 +33,11 @@ export const ChatStudyConnect = () => {
           </div>
           <div className="flex flex-col items-center gap-[21px] pb-[33px]">
             <div>
-              <button>
+              <button className="bg-[#17153A] w-[65px] h-[65px] rounded-full flex justify-center">
                 <img
-                  src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1676329558/TalkThru/Landing%20Page/down_arrow_kf61rx.png"
+                  src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1676767452/TalkThru/Landing%20Page/down_vector_m7rq5j.png"
                   alt="down-button"
+                  className="animate-updown transform origin-bottom"
                 />
               </button>
             </div>
