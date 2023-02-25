5a1ef804756f417b84f29e6b0660993126c336f5
Peter Yoo <trustintofu@gmail.com>
2023-02-24 16:34:44 -0800

    feat(MatchMe): Adds demo to fetch individual user data on button click.

--- src/pages/MatchMe.pages.tsx
+++ src/pages/MatchMe.pages.tsx
@@ -1,14 +1,52 @@
 import { useState } from 'react';
-import { ChooseExpertise } from '../components/pre-meeting/ChooseExpertise.components';
-import { ChooseSubject } from '../components/pre-meeting/ChooseSubject.components';
-import { LookingForUser } from '../components/pre-meeting/LookingForUser.components';
+import { ChooseExpertise } from '../components/match-me/ChooseExpertise.components';
+import { ChooseSubject } from '../components/match-me/ChooseSubject.components';
+import { LookingForUser } from '../components/match-me/LookingForUser.components';
+
+// Set the URL of the backend server
+const server_url = import.meta.env.VITE_BACKEND_URL as string;
 
 export const MatchMePage = () => {
   const [subject, setSubject] = useState<string>('');
   const [expertise, setExpertise] = useState<string>('');
+  const [userData, setUserData] = useState<any>({});
 
+  const handleFetchUserData = async () => {
+    try {
+      const token = localStorage.getItem('token');
+      if (!token) {
+        console.log('Token not found');
+        return;
+      }
+      const response = await fetch(`${server_url}/user`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const result = await response.json();
+      if (response.ok) {
+        setUserData(result);
+        console.log(result);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <>
+      {/* Fetch user Data test */}
+      <div className="flex flex-col items-center justify-center pt-10">
+        <div>
+          <button
+            onClick={() => handleFetchUserData()}
+            className="rounded-lg border border-gray-700 p-3"
+          >
+            Fetch Data
+          </button>
+        </div>
+        <p>User Id: {userData.id}</p>
+      </div>
+      {/*  */}
       <div className="h-screen w-full">
         {/* Choose a Subject */}
         {subject === '' && <ChooseSubject setSubject={setSubject} />}
