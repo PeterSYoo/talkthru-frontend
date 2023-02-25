52f292eed7a10c90c6151413d46ec41611d87880
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-24 16:08:11 -0800

    style(tailwind.config): Adds custom module for 'Archivo' font family.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
 			worksans: ['Work Sans'],
 			poppins: ['Poppins'],
 			openSans: ['Open Sans'],
+			archivo: ['Archivo'],
 		},
 		boxShadow: {
 			'meeting-room': '0px 4px 4px 0px rgba(0, 0, 0, 0.25)',
