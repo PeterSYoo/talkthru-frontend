c8ef5613ef10ce7f98c60707b2cede719fd9e905
Peter Yoo <trustintofu@gmail.com>
2023-02-26 13:34:32 -0800

    feat(MatchMe): Creates demo to match users.

--- src/pages/MatchMe.pages.tsx
+++ src/pages/MatchMe.pages.tsx
@@ -10,34 +10,14 @@ const server_url = import.meta.env.VITE_BACKEND_URL as string;
 export const MatchMePage = () => {
   const [subject, setSubject] = useState<string>('');
   const [expertise, setExpertise] = useState<string>('');
-  const { userId } = useContext(UserContext);
-  const [userSubject, setUserSubject] = useState<string>('');
-  const [userExpertise, setUserExpertise] = useState<string>('');
+  const { userId, subject: userSubject } = useContext(UserContext);
+  const [matchedUser, setMatchedUser] = useState<any>();
 
-  const handleUpdateSubject = async (id: number, subject: string) => {
-    try {
-      const response = await fetch(`${server_url}/matching/choose-subject`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          id,
-          subject,
-        }),
-      });
-      const result = await response.json();
-
-      if (response.ok) {
-        setUserSubject(result.subject);
-        console.log(result);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const handleUpdateExpertise = async (id: number, expertise: string) => {
+  const handleMatchUser = async (
+    subject: string,
+    id: number,
+    expertise: string
+  ) => {
     try {
       const response = await fetch(`${server_url}/matching/choose-expertise`, {
         method: 'PUT',
@@ -45,6 +25,7 @@ export const MatchMePage = () => {
           'Content-Type': 'application/json',
         },
         body: JSON.stringify({
+          subject,
           id,
           expertise,
         }),
@@ -52,7 +33,7 @@ export const MatchMePage = () => {
       const result = await response.json();
 
       if (response.ok) {
-        setUserExpertise(result.expertise);
+        setMatchedUser(result);
         console.log(result);
       }
     } catch (error) {
@@ -63,33 +44,14 @@ export const MatchMePage = () => {
   return (
     <>
       {/* Demo for updating user subject and expertise on backend */}
-      <div className="mx-auto mt-5 grid grid-rows-[1fr_1fr] justify-center gap-5 rounded-lg border border-gray-400 p-3">
-        <div className="grid grid-cols-[1fr_1fr] gap-20">
-          <button
-            onClick={() => handleUpdateSubject(userId, 'Computer Science')}
-            className="rounded-lg border border-gray-400 p-3"
-          >
-            Update Subject: Computer Science
-          </button>
-          <button
-            onClick={() => handleUpdateExpertise(userId, 'Beginner')}
-            className="rounded-lg border border-gray-400 p-3"
-          >
-            Update Expertise: Beginner
-          </button>
-        </div>
-        <div className="grid grid-cols-[1fr_1fr] gap-20">
-          {/* Subject */}
-          <div className="mx-auto grid w-full grid-cols-[1fr_2fr]">
-            <h1 className="font-medium">Subject:&nbsp;</h1>
-            <p className="text-blue-500">{userSubject || 'n/a'}</p>
-          </div>
-          {/* Expertise */}
-          <div className="mx-auto grid w-full grid-cols-[1fr_2fr]">
-            <h1 className="font-medium">Expertise:&nbsp;</h1>
-            <p className="text-blue-500">{userExpertise || 'n/a'}</p>
-          </div>
-        </div>
+      <div className="mt-3 flex flex-col items-center gap-3">
+        <button
+          onClick={() => handleMatchUser(userSubject, userId, 'Intermediate')}
+          className="border border-gray-400 p-3"
+        >
+          Find User to Match
+        </button>
+        <div className="font-medium">Name: {matchedUser?.name || 'na'}</div>
       </div>
       {/*  */}
       <div className="h-screen w-full">
