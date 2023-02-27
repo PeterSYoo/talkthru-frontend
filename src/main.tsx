46abb36e429f8139424cd3839ef9f12ad2eab09a
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-02-27 10:58:18 -0800

    refactor(PageRoutes): Moves context providers from main to wrap specific routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/UserContext';
-import { RoomProvider } from './contexts/RoomContext';
 import { PageRoutes } from './PageRoutes';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <BrowserRouter>
-    <UserProvider>
-      <RoomProvider>
-        <PageRoutes />
-      </RoomProvider>
-    </UserProvider>
-  </BrowserRouter>
+	<BrowserRouter>
+		<UserProvider>
+			<PageRoutes />
+		</UserProvider>
+	</BrowserRouter>
 );
