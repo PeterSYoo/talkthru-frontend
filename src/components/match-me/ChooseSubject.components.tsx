2114183c5d34bd105b04b5765bd6b79950b178d8
Peter Yoo <trustintofu@gmail.com>
2023-03-01 20:40:13 -0800

    feat(ChooseSubject): Creates carousels for users to search for subjects.

--- src/components/match-me/ChooseSubject.components.tsx
+++ src/components/match-me/ChooseSubject.components.tsx
@@ -1,65 +1,65 @@
+import 'swiper/css';
+import { ArtsAndHumanities } from './choose-subject/ArtsAndHumanities.components';
+import { BusinessAndEconomicDevelopment } from './choose-subject/BusinessAndEconomicDevelopment.components';
+import { STEM } from './choose-subject/STEM.components';
+import { TrendingNow } from './choose-subject/TrendingNow.components';
+
 export const ChooseSubject = ({
   setSubject,
 }: {
   setSubject: (subject: any) => void;
 }) => {
   return (
-		<>
-			<div className='mx-auto flex max-w-[1440px] flex-col gap-[82px] px-[105px] pt-[106px] pb-[313px]'>
-				<div className='flex flex-col gap-[100px]'>
-					{/* Step 1 of 2 */}
-					<div>
-						<span className='text-[22px] font-medium'>Step 1 of 2</span>
-						<h1 className='text-[40px] font-bold'>Find a field of study</h1>
-						<span className='text-[18px] font-medium'>
-							Select up to 1 field of study that pertains to your desired study session.
-						</span>
-					</div>
-					{/* Browse by field of study */}
-					<div>
-						<span className='text-[22px] font-bold'>Browse by field of study</span>
-					</div>
-				</div>
-				{/* Subjects */}
-				<div className='mx-auto flex flex-wrap gap-[122px]'>
-					{/* Business */}
-					<button
-						onClick={() => setSubject('Business')}
-						className='flex h-[216px] w-[216px] items-center justify-center rounded-[11px] shadow-[0px_2px_5px_rgba(0,0,0,0.6)]'>
-						<img
-							src='https://res.cloudinary.com/dryh1nvhk/image/upload/v1676616390/TalkThru/Waiting%20Room%20Page/Choose%20Subject/business_mmt9ab.png'
-							alt='business'
-						/>
-					</button>
-					{/* Nursing */}
-					<button
-						onClick={() => setSubject('Nursing')}
-						className='flex h-[216px] w-[216px] items-center justify-center rounded-[11px] shadow-[0px_2px_5px_rgba(0,0,0,0.6)]'>
-						<img
-							src='https://res.cloudinary.com/dryh1nvhk/image/upload/v1676616390/TalkThru/Waiting%20Room%20Page/Choose%20Subject/nursing_fahq8v.png'
-							alt='nursing'
-						/>
-					</button>
-					{/* Biomedical */}
-					<button
-						onClick={() => setSubject('Biomedical Sciences')}
-						className='flex h-[216px] w-[216px] items-center justify-center rounded-[11px] shadow-[0px_2px_5px_rgba(0,0,0,0.6)]'>
-						<img
-							src='https://res.cloudinary.com/dryh1nvhk/image/upload/v1676616390/TalkThru/Waiting%20Room%20Page/Choose%20Subject/biomedical_w43pfq.png'
-							alt='biomedical'
-						/>
-					</button>
-					{/* Biology */}
-					<button
-						onClick={() => setSubject('Biology')}
-						className='flex h-[216px] w-[216px] items-center justify-center rounded-[11px] shadow-[0px_2px_5px_rgba(0,0,0,0.6)]'>
-						<img
-							src='https://res.cloudinary.com/dryh1nvhk/image/upload/v1676616390/TalkThru/Waiting%20Room%20Page/Choose%20Subject/biology_tb0wf6.png'
-							alt='biology'
-						/>
-					</button>
-				</div>
-			</div>
-		</>
-	);
+    <>
+      <div className="grid h-screen grid-rows-[440px_1fr_90px]">
+        {/* Row 1 */}
+        <div className="w-full bg-gradient-to-b from-[#8B8A9C] to-white">
+          <div className="mx-auto flex w-full max-w-[1026px] flex-col items-center gap-[31px] px-5 pt-[89px]">
+            <h1 className="flex w-full justify-start text-[40px] font-bold leading-[60px]">
+              Discover topics
+              <br /> for your TalkThru study session
+            </h1>
+            <p className="text-[20px] font-medium">
+              From graphic art to computer science, from legal studies to
+              biology, from accounting to physics, TalkThru offers a wide
+              variety of academic subjects. Select up to 1 field of study that
+              pertains to your desired session.
+            </p>
+          </div>
+        </div>
+        {/* Row 2 */}
+        <div className="mx-auto flex w-full max-w-[1280px] flex-col gap-[52px] px-5">
+          {/* Trending Now Carousel */}
+          <TrendingNow setSubject={setSubject} />
+          {/* Browse by Subject Type */}
+          <div className="flex flex-col items-center gap-[37px] pt-[36px]">
+            <p className="text-[27px] font-bold">Browse by Subject Type</p>
+          </div>
+          {/* Arts and Humanities */}
+          <div className="flex flex-col items-center gap-[21px]">
+            <p className="flex w-full justify-start text-[22px] font-medium">
+              Arts and Humanities
+            </p>
+            <ArtsAndHumanities setSubject={setSubject} />
+          </div>
+          {/* Sciences, Math, Engineering, and Technology */}
+          <div className="flex flex-col items-center gap-[37px] pt-[36px]">
+            <p className="flex w-full justify-start text-[22px] font-medium">
+              Sciences, Math, Engineering, and Technology
+            </p>
+            <STEM setSubject={setSubject} />
+          </div>
+          {/* Business and Economic Development  */}
+          <div className="flex flex-col items-center gap-[37px] pt-[36px]">
+            <p className="flex w-full justify-start text-[22px] font-medium">
+              Business and Economic Development
+            </p>
+            <BusinessAndEconomicDevelopment setSubject={setSubject} />
+          </div>
+        </div>
+        {/* Empty Row */}
+        <div></div>
+      </div>
+    </>
+  );
 };
