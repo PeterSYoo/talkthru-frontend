8547475ba675f961f57f23abb1e25203845de2b1
Tyson Gomes <tkg@TKG-MacBook-Pro.local>
2023-03-02 18:33:14 -0800

    feat(MatchMe): Adds logic to handle conditional rendering up until user is navigated to the video chat.

--- src/pages/MatchMe.pages.tsx
+++ src/pages/MatchMe.pages.tsx
@@ -5,7 +5,6 @@ import { ChooseExpertise } from '../components/match-me/ChooseExpertise.componen
 import { ChooseSubject } from '../components/match-me/choose-subject/ChooseSubject.components';
 import { LookingForUser } from '../components/match-me/LookingForUser.components';
 import { UserContext } from '../contexts/UserContext';
-import { webSocket } from '../webSocket';
 
 // Set the URL of the backend server
 const server_url = import.meta.env.VITE_BACKEND_URL as string;
@@ -15,71 +14,72 @@ export const MatchMePage = () => {
 	const [selectedSubject, setSelectedSubject] = useState<string>('');
 	const [selectedExpertise, setSelectedExpertise] = useState<string>('');
 	const { userData, setUserData, handleFetchUserData, userId, userName } = useContext(UserContext);
-	const [matchedUser, setMatchedUser] = useState<any>();
 	const [canSearch, setCanSearch] = useState<boolean>(false);
 	const [isSearching, setIsSearching] = useState<boolean>(false);
 
-  const handleUpdateSubject = async (id: string, subject: string) => {
-    console.log('handleUpdateSubject Called');
-    try {
-      const response = await fetch(`${server_url}/matching/choose-subject`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, subject }),
-      });
+	const handleUpdateSubject = async (id: string, subject: string) => {
+		console.log('handleUpdateSubject Called');
+		try {
+			const response = await fetch(`${server_url}/matching/choose-subject`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ id, subject }),
+			});
 
-      if (response.ok) {
-        const result = await response.json();
-        console.log({ result });
-        setUserData(result);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			if (response.ok) {
+				const result = await response.json();
+				console.log({ result });
+				setUserData(result);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const handleUpdateExpertise = async (id: string, expertise: string) => {
-    console.log('handleUpdateExpertise Called');
-    try {
-      const response = await fetch(`${server_url}/matching/choose-expertise`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, expertise }),
-      });
+	const handleUpdateExpertise = async (id: string, expertise: string) => {
+		console.log('handleUpdateExpertise Called');
+		try {
+			const response = await fetch(`${server_url}/matching/choose-expertise`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ id, expertise }),
+			});
 
-      if (response.ok) {
-        const result = await response.json();
-        console.log({ result });
-        setUserData(result);
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			if (response.ok) {
+				const result = await response.json();
+				console.log({ result });
+				setUserData(result);
+			}
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
-  const handleMatchUser = async (userId: string) => {
-    console.log('handleMatchingUser Called');
-    setIsSearching(true);
-    try {
-      const response = await fetch(`${server_url}/matching/match-user`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id: userId }),
-      });
+	const handleMatchUser = async (userId: string) => {
+		console.log('handleMatchingUser Called');
+		setSelectedSubject('');
+		setSelectedExpertise('');
+		setIsSearching(true);
+		try {
+			const response = await fetch(`${server_url}/matching/match-user`, {
+				method: 'PUT',
+				headers: {
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify({ id: userId }),
+			});
 
-      const result = await response.json();
-      console.log({ result });
-      navigate(`/room/${result}`);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+			const result = await response.json();
+			console.log({ result });
+			navigate(`/room/${result}`);
+		} catch (error) {
+			console.error(error);
+		}
+	};
 
 	useEffect(() => {
 		handleFetchUserData();
@@ -89,63 +89,46 @@ export const MatchMePage = () => {
 		handleUpdateSubject(userId, selectedSubject);
 	}, [selectedSubject]);
 
-  useEffect(() => {
-    handleUpdateExpertise(userId, selectedExpertise);
-  }, [selectedExpertise]);
+	useEffect(() => {
+		handleUpdateExpertise(userId, selectedExpertise);
+	}, [selectedExpertise]);
 
-  useEffect(() => {
-    setCanSearch(selectedSubject !== '' && selectedExpertise !== '');
-  }, [selectedSubject, selectedExpertise]);
+	useEffect(() => {
+		setCanSearch(selectedSubject !== '' && selectedExpertise !== '');
+	}, [selectedSubject, selectedExpertise]);
 
-  // console.log({ userData });
-  // console.log({ selectedSubject });
-  // console.log({ selectedExpertise });
-  // console.log({ canSearch });
-  // console.log({ isSearching });
+	// console.log({ userData });
+	// console.log({ selectedSubject });
+	// console.log({ selectedExpertise });
+	// console.log({ canSearch });
+	// console.log({ isSearching });
 
-  return (
-    <>
-      <div className="w-full">
-        {/* Choose a Subject */}
-        {selectedSubject === '' && (
-          <ChooseSubject setSubject={setSelectedSubject} />
-        )}
-        {/* Choose Expertise */}
-        {selectedExpertise === '' && (
-          <>
-            {selectedSubject !== '' && (
-              <ChooseExpertise
-                subject={selectedSubject}
-                setSubject={setSelectedSubject}
-                expertise={selectedExpertise}
-                setExpertise={setSelectedExpertise}
-              />
-            )}
-          </>
-        )}
-        {/* Looking for User */}
-        {isSearching && (
-          <LookingForUser
-            setExpertise={setSelectedExpertise}
-            subject={selectedSubject}
-          />
-        )}
+	return (
+		<>
+			<div className='w-full'>
+				{/* Choose a Subject */}
+				{!isSearching && selectedSubject === '' && (
+					<ChooseSubject setSubject={setSelectedSubject} />
+				)}
+				{/* Choose Expertise */}
 
-        {/* Demo for updating user subject and expertise on backend */}
-        {/* <div className="mt-3 flex flex-col items-center gap-3">
-          {userData && userData.name}
-          {canSearch && (
-            <button
-              onClick={() => handleMatchUser(userId)}
-              className="border border-gray-400 p-3"
-            >
-              Find User to Match
-            </button>
-          )}
-          <div className="font-medium">Name: {matchedUser?.name || 'na'}</div>
-          {matchedUser && <CreateButton onClick={handleMatchUser} />}
-        </div> */}
-      </div>
-    </>
-  );
+				{!isSearching && selectedSubject !== '' && (
+					<ChooseExpertise
+						userId={userId}
+						subject={selectedSubject}
+						setSubject={setSelectedSubject}
+						expertise={selectedExpertise}
+						setExpertise={setSelectedExpertise}
+						handleMatchUser={handleMatchUser}
+						canSearch={canSearch}
+					/>
+				)}
+
+				{/* Looking for User */}
+				{isSearching && (
+					<LookingForUser setExpertise={setSelectedExpertise} subject={selectedSubject} />
+				)}
+			</div>
+		</>
+	);
 };
